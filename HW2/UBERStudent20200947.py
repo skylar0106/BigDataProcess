9beb77fb2a520356301cdec65ed7dec76628d118
dahye <color4541@naver.com>
2023-11-08 14:20:05 +0900

    11/8

--- HW2/UBERStudent20200947.py
+++ HW2/UBERStudent20200947.py
@@ -14,20 +14,14 @@ region_list = []
 
 with open(input_file) as file:
     for line in file:
-        cnt = 1
-        for r in line.split(","):
-            if cnt % 4 == 1:
-                region_list.append(r)
-            cnt += 1
-
-region_list = list(set(region_list))
+        r = line.split(",")
+        region_list.append(r[0])
+            
 region_dict = {}
-day_dict = {"MON": [0, 0], "TUE": [0, 0], "WED": [0, 0], "THU": [0, 0], "FRI": [0, 0], "SAT": [0, 0], "SUN": [0, 0]}
 for r in region_list:
-    region_dict[r] = day_dict
+    region_dict[r] = {"MON": [0, 0], "TUE": [0, 0], "WED": [0, 0], "THU": [0, 0], "FRI": [0, 0], "SAT": [0, 0], "SUN": [0, 0]}
 
 for r in region_dict:
-    list = [0, 0]
     with open(input_file) as file:
         for line in file:
             if r in line:
@@ -37,10 +31,11 @@ for r in region_dict:
                 date = datetime.strptime(l[1], date_format)
                         
                 day = what_day_is_it(date)
+                tmp_dict = region_dict[r]
+                 
+                tmp_dict[day][0] += int(l[2])
+                tmp_dict[day][1] += int(l[3])
                 
-                region_dict[r][day][0] += int(l[2])
-                region_dict[r][day][1] += int(l[3])
-
 with open(output_file, "w", encoding="utf-8") as file:
     for r in region_dict:
         for d in days:
